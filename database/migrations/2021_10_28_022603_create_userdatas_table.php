f435d5951ebbfde6c1b966dddf8e0b358a72b225
Santi <it.santiagotoledo@gmail.com>
2021-10-27 23:35:09 -0300

    create models and migrations

---
+++ database/migrations/2021_10_28_022603_create_userdatas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserdatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('userdatas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('iduser');
+            $table->string('name', 128);
+            $table->string('photo', 256)->nullable();
+            $table->string('age');
+            $table->string('about');
+            $table->string('genre', 64);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('userdatas');
+    }
+}
---
+++ database/migrations/2021_10_28_022627_create_confirmations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfirmationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirmations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('iduser');
+            $table->integer('idactivity');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confirmations');
+    }
+}
