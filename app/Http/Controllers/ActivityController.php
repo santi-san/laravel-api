2842e42b988061c0267f0bada68684c43d3c11eb
Santi <it.santiagotoledo@gmail.com>
2021-10-28 22:06:09 -0300

    method to add an activity

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Activity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -22,5 +23,31 @@ class ActivityController extends ApiController
         $data['activities'] = $activities;
 
         return $this->sendResponse($data, 'List of activities');
+    }
+
+    public function addActivity(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:activities',
+            'photo' => 'required',
+            'description' => 'required',
+            'date' => 'required'
+        ]);
+        // Si falla la validacion devuelve json con status 422
+        if($validator->fails()) {
+            return $this->sendError($validator->errors(),"Error en la validacion de datos",422); 
+        }
+        
+        //create activity
+        $new_activity = new Activity();
+        $new_activity->name = $request->get('name');
+        $new_activity->photo = $request->get('photo');
+        $new_activity->description = $request->get('description');
+        $new_activity->date = $request->get('date');
+        $new_activity->save();
+
+        $data = [
+            'activity' => $new_activity,
+        ];
+        return $this->sendResponse($data, 'Activity created successfully');
     }
 }
